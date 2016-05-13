9b33bf94f949def9bd80660e008b613f4a34b2b8
maocl023 <maocl023@gmail.com>
2016-05-13 20:56:36 +0800

    project initial commit

---
+++ Minsta/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Minsta
+//
+//  Created by maocl023 on 16/5/12.
+//  Copyright © 2016年 jjj2mdd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder
+
+@end
+
---
+++ Minsta/AppDelegate.m
@@ -0,0 +1,36 @@
+//
+//  AppDelegate.m
+//  Minsta
+//
+//  Created by maocl023 on 16/5/12.
+//  Copyright © 2016年 jjj2mdd. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate () <UIApplicationDelegate>
+{
+    UIWindow *_window;
+}
+@end
+
+@implementation AppDelegate
+
+#pragma mark - UIApplicationDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {}
+
+- (void)applicationWillTerminate:(UIApplication *)application {}
+
+@end
